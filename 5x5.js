b120e85bfb88893d9f332c823f4ee6caede6e680
Shri-10 <67466287+Shri-10@users.noreply.github.com>
2020-07-24 19:14:06 +0530

    Update 5x5.js

--- 5x5.js
+++ 5x5.js
@@ -1,4 +1,5 @@
 const tictactoeGame=new TicTacToeGame();
+// Starts the game
 tictactoeGame.start();
 function TicTacToeGame(){
   document.querySelector('.endgame').style.display = 'none';
@@ -6,17 +7,22 @@ function TicTacToeGame(){
   for(var i = 0; i < arr.length; i++) {
     arr[i].innerText = '';
     arr[i].style.removeProperty('background-color');}
+  // Declaring the OOP's to function container
   const board=new Board();
   const humanPlayer=new HumanPlayer(board);
   const computerPlayer=new ComputerPlayer(board);
+  // Assigning turn to zero for huan to start
   let turn=0;
   this.start=function(){
     const config={childList:true};
+    // Observes when each move is made in the cell
     const observer=new MutationObserver(()=>takeTurn());
     board.positions.forEach((el)=>observer.observe(el,config));
+    // Calls this function for human and computer to make their turns
     takeTurn();
     }
   function takeTurn(){
+    // Checks for win
     if (board.checkForWinner()==5){
       document.querySelector('.endgame').style.display = 'block';
       document.querySelector('.endgame .text').innerText = 'You Lost!';
@@ -28,13 +34,14 @@ function TicTacToeGame(){
       document.querySelector('.endgame .text').innerText = 'You Won!';
       turn=0;
       return true ;
-    }
+    }// Checks for tie
     else if (board.checkForTie(board)){
       document.querySelector('.endgame').style.display = 'block';
       document.querySelector('.endgame .text').innerText = 'Tie';
       turn=0;
       return true;
-    }if (turn<25){
+    }// Calling human and computer functions to make their moves back to back.
+    if (turn<25){
     if (turn%2===0){
       humanPlayer.takeTurn();
     }else if(turn%2!==0){
@@ -47,6 +54,7 @@ function TicTacToeGame(){
 
 
 function Board(){
+  // Getting all the positions from the cells
   this.positions=Array.from(document.querySelectorAll('.col-xs-4'));
   this.checkForWinner=function(){
     let winner=false;
@@ -91,6 +99,7 @@ function Board(){
   }
   this.checkForTie=function(board){
     const arr=Array.from(document.querySelectorAll('.col-xs-4'));
+    // Getting the available positions in the cells
     const availablePositions = board.positions.filter((p)=>p.innerText==='');
     if (availablePositions.length==0){
       for (i=0;i<arr.length;i++){
@@ -112,10 +121,12 @@ function HumanPlayer(board){
       const availablePositions = board.positions.filter((p)=>p.innerText==='');
       for (i=0;i<availablePositions.length;i++){
         if (availablePositions[i]===event.target){
+          // Gets the move which human made and records it.
           event.target.innerText='X';
           board.positions.forEach(el=>el.removeEventListener('click',handleTurnTaken));
           const availablePositions = board.positions.filter((p)=>p.innerText==='');
           var k=0;
+          // To make the joker appear in random spots
           if (availablePositions.length>10){
             const occupiedposj=board.positions.filter((p)=>p.innerText==='J');
             if (occupiedposj.length>0){
@@ -149,182 +160,4 @@ function ComputerPlayer(board){
     availablePositions[move].innerText='O';
   }
 }
-function ComputerPlayer1(board){
-  this.takeTurn = function(){
-    var array=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24];
-    var array1=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24];
-    const arr=Array.from(document.querySelectorAll('.col-xs-4'));
-    const availablePositions = board.positions.filter((p)=>p.innerText==='');
-    const occupiedPositionsX = board.positions.filter((p)=>p.innerText==='X');
-    const occupiedPositionsO = board.positions.filter((p)=>p.innerText==='O');
-    const occupiedPositionsJ = board.positions.filter((p)=>p.innerText==='Joker');
-    for(i=0;i<arr.length;i++){
-      for(j=0;j<occupiedPositionsX.length;j++){
-        if (arr[i]==occupiedPositionsX[j]){
-          var x="X";
-          array.splice(i,1,x);
-          array1.splice(i,1,x);
-        }
-      }
-    }
-    for(i=0;i<arr.length;i++){
-      for(j=0;j<occupiedPositionsO.length;j++){
-        if (arr[i]==occupiedPositionsO[j]){
-          var y="O";
-          array.splice(i,1,y);
-          array1.splice(i,1,y);
 
-        }
-      }
-    }
-    for(i=0;i<arr.length;i++){
-      for(j=0;j<occupiedPositionsJ.length;j++){
-        if (arr[i]==occupiedPositionsJ[j]){
-          var v="Joker";
-          array.splice(i,1,v);
-          array1.splice(i,1,v);
-
-        }
-      }
-    }
-    for(i=0;i<arr.length;i++){
-      for(j=0;j<availablePositions.length;j++){
-        if (arr[i]==availablePositions[j]){
-          var z=availablePositions[j];
-          array1.splice(i,1,z);
-        }
-      }
-    }
-    var move=minimax(array,"O")[0];
-    array1[move].innerText='O';
-  }
-}
-function pos(arr){
-  var pos=[];
-  for(v=0;v<arr.length;v++){
-    if ((arr[v]!="X") && (arr[v]!="O") && (arr[v]!="Joker")){
-      pos.push(arr[v]);
-    }
-  }
-  return pos;
-}
-
-function minimax(array,turn){
-  console.log("yess")
-  var score;
-  if (turn=="X"){
-    var opponent="O";
-  }else{
-    opponent="X";
-  }
-  var best_pos;
-  var max_score=-10;
-  var p=pos(array);
-  var i; var j;
-  for(i=0;i<array.length;i++){
-    for(j=0;j<p.length;j++){
-      if (array[i]==p[j]){
-        array.splice(i,1,turn);
-        const ii=i;
-        const jj=j;
-        const k=p[j];
-        if (checkIfWon(array,turn)==1){
-          score=1;
-        }
-        else if (pos(array).length===0){
-          score=0;
-        }
-        else{
-          score=-minimax(array,opponent)[1];
-          console.log(score)
-        }
-        i=ii;
-        j=jj;
-        array.splice(i,1,p[j]);
-        if (score>max_score){
-          best_pos=p[j];
-          max_score=score;
-        }
-      }
-    }
-  }
-  const result=[];
-  result.splice(0,1,best_pos);
-  result.splice(1,1,max_score);
-  return result;
- }
- function checkIfWon(arr,g){
-   var posi=[];
-   var posj=[];
-   var p=0;
-   var q=0;
-   var h=0;
-   var k=0;var l=0;var r=0;var s=0;var t=0;
-   var newarray = [];
-   for(var i=0;i < arr.length;i =i+5){
-     newarray.push(arr.slice(i,i+5));}
-   for(i=0;i<3;i++){
-     for(j=0;j<3;j++){
-       if (newarray[i][j]==g){
-         posi.push(i);
-         posj.push(j);
-         if (i==j){
-           p=p+1;
-         }if ((i==j && i==2)||i-j==4||j-i==4||(i-j==3&&i==3)||(j-i==3&&j==3)){
-           q=q+1;
-         }
-       }
-     }
-   }
-   if (p==5){
-     h=1;
-   }else if (q==5){
-     h=1;
-   }
-   if (posi.length>=5){
-     for(i=0;i<posi.length;i++){
-       if (posi[i]==posi[i]){
-         if (posi[i]==0){
-           k=k+1;
-         }
-         else if(posi[i]==1){
-           l=l+1;
-         }else if (posi[i]==2){
-           r=r+1;
-         }else if (posi[i]==3){
-           s=s+1;
-       }else if (posi[i]==4){
-         t=t+1;
-     }
-   }}}
-   if (k==5||l==5||r==5||(s==5)||(t==5)){
-     h=1;
-   }
-   k=0;l=0;r=0;s=0;t=0;
-   if (posj.length>=5){
-     for(i=0;i<posj.length;i++){
-       if (posj[i]==posj[i]){
-         if (posj[i]==0){
-           k=k+1;
-         }
-         else if(posj[i]==1){
-           l=l+1;
-         }else if (posj[i]==2){
-           r=r+1;
-         }else if (posj[i]==3){
-           s=s+1;
-         }else if (posj[i]==4){
-           t=t+1;
-         }
-       }
-     }
-   }
-   if (k==5||l==5||r==5||s==5||t==5){
-     h=1;
-   }
-   if (h==1){
-     return 1;
-   }else{
-     return false;
-   }
- }
